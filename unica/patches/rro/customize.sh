salvogiangri/UN1CA

unica: follow up of 27664c2e
Change-Id: Iaf500441adfba9c5bf61f72be695880c140f8ba1

unica/patches/rro/customize.sh
@@ -1,5 +1,5 @@
 if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-    DECODE_APK "/product/overlay/framework-res__auto_generated_rro_product.apk"
+    DECODE_APK "product" "overlay/framework-res__auto_generated_rro_product.apk"
 
     echo "Applying stock overlay configs"
     rm -rf "$APKTOOL_DIR/product/overlay/framework-res__auto_generated_rro_product.apk/res"

gsilover-tr/Project-LighterDroid

Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

unica/patches/rro/customize.sh
@@ -1,5 +1,5 @@
 if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-    DECODE_APK "/product/overlay/framework-res__r12sxxx__auto_generated_rro_product.apk"
+    DECODE_APK "product" "overlay/framework-res__r12sxxx__auto_generated_rro_product.apk"
 
     echo "Applying stock overlay configs"
     rm -rf "$APKTOOL_DIR/product/overlay/framework-res__r12sxxx__auto_generated_rro_product.apk/res"
@@ -9,7 +9,7 @@ if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
 fi
 
 # TODO: Add a proper check if we need to remove this
-DECODE_APK "/product/overlay/SystemUI__r12sxxx__auto_generated_rro_product.apk"
+DECODE_APK "product" "overlay/SystemUI__r12sxxx__auto_generated_rro_product.apk"
 sed -i -e "/config_enableDisplayCutoutProtection/d" -e "/config_enableRoundedCorner/d" "$APKTOOL_DIR/product/overlay/SystemUI__r12sxxx__auto_generated_rro_product.apk/res/values/bools.xml"
 rm "$APKTOOL_DIR/product/overlay/SystemUI__r12sxxx__auto_generated_rro_product.apk/res/values/dimens.xml"
 rm "$APKTOOL_DIR/product/overlay/SystemUI__r12sxxx__auto_generated_rro_product.apk/res/values/public.xml"