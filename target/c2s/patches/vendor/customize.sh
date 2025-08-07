f3bd8453dbebab9b48f32b8875742020b07d425d
WalterGray <alenh944@gmail.com>
2025-08-08 00:19:23 +0400

    Update customize.sh Remove uwb as it causes crashes

--- target/c2s/patches/vendor/customize.sh
+++ target/c2s/patches/vendor/customize.sh
@@ -1,23 +1,17 @@
-LOG_STEP_IN "- Updating UWB HAL"
-
-DELETE_FROM_WORK_DIR "vendor" "etc/init/nxp-uwb-service.rc"
+LOG_STEP_IN "- Removing UWB Hal"
 
 BLOBS_LIST="
 bin/hw/vendor.samsung.hardware.uwb@1.0-service
-etc/libuwb-countrycode.conf
-etc/libuwb-feature.conf
+etc/uwb_key
 etc/libuwb-nxp.conf
 etc/libuwb-uci.conf
 etc/init/init.vendor.uwb.rc
-etc/init/vendor.samsung.hardware.uwb@1.0-service.rc
-firmware/uwb
-lib64/uwb_uci.hal.so
-lib64/libmemunreachable.so
+etc/init/nxp-uwb-service.rc
+etc/vintf/manifest/uwb-service.xml
 "
 for blob in $BLOBS_LIST
 do
-    ADD_TO_WORK_DIR "p3sxxx" "vendor" "$blob"
+    DELETE_FROM_WORK_DIR "vendor" "$blob"
 done
 
-SET_PROP "vendor" "ro.vendor.uwb.feature.chipname" "sr100"
 LOG_STEP_OUT
